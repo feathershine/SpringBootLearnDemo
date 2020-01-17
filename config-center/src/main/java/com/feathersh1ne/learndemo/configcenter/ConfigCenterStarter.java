65d184560618c0e4ee5cf02369715163922351d7
hailun.zhan <hailun.zhan@accenture.com>
2020-01-17 11:27:52 +0800

    init project

---
+++ config-center/src/main/java/com/feathersh1ne/learndemo/configcenter/ConfigCenterStarter.java
@@ -0,0 +1,11 @@
+package com.feathersh1ne.learndemo.configcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigCenterStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterStarter.class);
+    }
+}
